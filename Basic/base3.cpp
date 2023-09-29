790b3777c34a4fb0524fd4ed557d99fb84f895b8
MdShazidAlHasan <mdshazid121@gmail.com>
2023-09-30 03:03:49 +0600

    added base2 and base3

---
+++ Basic/base3.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll long long 
+#define all(x) x.begin(), x.end()
+
+using namespace std;
+
+int main(){
+    int t = 1;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        
+    }  
+}
