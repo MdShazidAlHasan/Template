97481e0bc9bc8883132efbf9f7ffb4a8e160ddad
Shazid <Milgate#313>
2026-09-19 20:38:22 +0600

    ncr code fixed

--- Combinatorics/nCr.cpp
+++ Combinatorics/nCr.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
+#define int long long
 const int MAXN = 1e6;
 const int MOD = 1e9+7;
-ll fac[MAXN], infac[MAXN];
+int fac[MAXN], infac[MAXN];
 
-ll expo(ll a, ll b){
+int expo(int a, int b){
     if(b==0){
         return 1;
     }
     if(b%2==0){
-        ll x = expo(a, b/2);
+        int x = expo(a, b/2);
         return (x*x)%MOD;
     }else{
-        ll x = expo(a, b/2);
-        ll ans = (x*x)%MOD;
+        int x = expo(a, b/2);
+        int ans = (x*x)%MOD;
         return (ans*a)%MOD;
     }
 }
 
-ll inverse(ll a){
+int inverse(int a){
     return expo(a, MOD-2);
 }
 
@@ -34,16 +34,16 @@ void compute_factorials(){
 } 
 
 // nCr = n! / r! * (n - r)!
-ll nCr(ll n, ll r){
+int nCr(int n, int r){
     if(n<0 or r<0 or r>n){
         return 0;
     }
-    ll num = fac[n];
-    ll den = (infac[r])*(infac[n-r])%MOD;
+    int num = fac[n];
+    int den = (infac[r])*(infac[n-r])%MOD;
     return (num*den)%MOD;
 }
 
-int main(){
+signed main(){
     compute_factorials();
     int n, m;cin>>n>>m;
     cout<<nCr(m, n)<<endl;
